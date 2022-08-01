f78a08ce136d9e927b99fbba9379183c6aea21c8
Sergey Sedoy <sergey.sedoy.97@icloud.com>
2022-08-01 14:54:32 +0300

    feat(SecurityContextTenantAware): `OidcUser` support

--- hawkbit-security-core/src/main/java/org/eclipse/hawkbit/security/SecurityContextTenantAware.java
+++ hawkbit-security-core/src/main/java/org/eclipse/hawkbit/security/SecurityContextTenantAware.java
@@ -24,6 +24,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.context.SecurityContextImpl;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 
 /**
  * A {@link TenantAware} implementation which retrieves the ID of the tenant
@@ -73,6 +74,8 @@ public class SecurityContextTenantAware implements TenantAware {
             final Object principal = context.getAuthentication().getPrincipal();
             if (principal instanceof UserPrincipal) {
                 return ((UserPrincipal) principal).getUsername();
+            } else if (principal instanceof OidcUser) {
+                return ((OidcUser) principal).getPreferredUsername();
             }
         }
         return null;
